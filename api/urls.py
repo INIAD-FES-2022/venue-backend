3294d7ad679b62f4f51344641d6f94bbb133237c
Naoto Matsumoto <s1F102001243@iniad.org>
2022-08-23 18:36:36 +0900

    fix: api/urls.py config/settings.py URLのパス部分を仕様書に合致するように調整しました

--- api/urls.py
+++ api/urls.py
@@ -1,15 +1,16 @@
 from django.urls import path, include
 from . import apis
+from django.conf import settings
+from django.contrib.staticfiles.urls import static
 from rest_framework import routers
 
 router = routers.DefaultRouter()
 router.register('program', apis.ProgramViewSet)
 router.register('notice', apis.NoticeViewSet)
 router.register('group', apis.GroupViewSet)
-router.register('category', apis.CategoryViewSet)
-router.register('image', apis.ImageViewSet)
-router.register('link', apis.LinkViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
