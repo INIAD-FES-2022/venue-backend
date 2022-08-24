cf422692d2cfe93910fa80a7c35dce71f4022581
Naoto Matsumoto <s1F102001243@iniad.org>
2022-08-24 15:28:55 +0900

    fix: api/admin.py admin画面からモデルの情報の登録ができるようにしました

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+class GroupAdmin(admin.ModelAdmin):
+    pass
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+class NoticeAdmin(admin.ModelAdmin):
+    pass
+
+class ProgramAdmin(admin.ModelAdmin):
+    pass
+
+class LinkAdmin(admin.ModelAdmin):
+    pass
+
+class ImageAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(models.Group, GroupAdmin)
+admin.site.register(models.Category, CategoryAdmin)
+admin.site.register(models.Notice, NoticeAdmin)
+admin.site.register(models.Program, ProgramAdmin)
+admin.site.register(models.Link, LinkAdmin)
+admin.site.register(models.Image, ImageAdmin)
