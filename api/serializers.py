780d1fdfb699fb95252e33d77fd22b27f99f459a
Naoto Matsumoto <s1F102001243@iniad.org>
2022-09-14 03:53:57 +0900

    fixed: syntax error

--- api/serializers.py
+++ api/serializers.py
@@ -30,7 +30,8 @@ class NoticeSerializer(serializers.ModelSerializer):
     category = serializers.SerializerMethodField()
 
     class Meta:
-        model = Notice fields = ["uuid", "title", "date", "category"] 
+        model = Notice
+        fields = ["uuid", "title", "date", "category"] 
     def get_category(self, obj):
         try:
             cats = obj.category.all()
