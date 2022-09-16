8d97cb708ee0135c0797dfe16aa073ecdb6bce13
Naoto Matsumoto <s1F102001243@iniad.org>
2022-09-16 09:35:32 +0900

    FIX settings.py: .envにDB名やユーザ名も含むように変更

--- config/settings.py
+++ config/settings.py
@@ -92,8 +92,8 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
-        'USER': 'postgres',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USERNAME'),
         'PASSWORD': env('DB_PASSWORD'),
         'HOST': 'db',
         'PORT': 5432,
