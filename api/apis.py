c060ff09e1684fc19efa40b05ce5d82194b8d056
Takuho3650 <s1f102101536@iniad.org>
2022-09-26 15:29:55 +0900

    update apis.py
    重複を無理矢理解消するように

--- api/apis.py
+++ api/apis.py
@@ -2,6 +2,7 @@ from django_filters.rest_framework import DjangoFilterBackend
 from .models import Program, Notice, Group
 from rest_framework import viewsets, filters
 from .serializers import ProgramSerializer, DetailProgramSerializer, NoticeSerializer, DetailNoticeSerializer, GroupSerializer, DetailGroupSerializer
+from django.db.models import Case, When
 
 class ProgramViewSet(viewsets.ModelViewSet):
     serializer_class = ProgramSerializer
@@ -9,13 +10,36 @@ class ProgramViewSet(viewsets.ModelViewSet):
     http_method_names = ["get"]
     filter_backends = [filters.OrderingFilter, DjangoFilterBackend]
     filterset_fields = ['category', 'place', 'start_at', 'end_at', 'group']
-    ordering_fields = ['start_at', 'end_at', 'title', 'group', 'category']
-    ordering = ['title']
+    ordering_fields = ['start_at', 'end_at', 'title', 'group']
     def get_serializer_class(self, *args, **kwargs):
         if self.action!='list':
             return DetailProgramSerializer
         else:
             return self.serializer_class
+    def get_queryset(self):
+        if not self.request.query_params.get("sort-by"):
+            return Program.objects.order_by("title")
+        else:
+            if self.request.query_params["sort-by"]=="category":
+                qs = Program.objects.order_by("category")
+                pk_list = []
+                for q in qs:
+                    if str(q.uuid) not in pk_list:
+                        pk_list.append(str(q.uuid))
+                preserved = Case(*[When(uuid=val, then=pos) for pos, val in enumerate(pk_list)], default=len(pk_list))
+                qs_out = Program.objects.filter(uuid__in=pk_list).order_by(preserved)
+                return qs_out
+            elif self.request.query_params["sort-by"]=="-category":
+                qs = Program.objects.order_by("-category")
+                pk_list = []
+                for q in qs:
+                    if str(q.uuid) not in pk_list:
+                        pk_list.append(str(q.uuid))
+                preserved = Case(*[When(uuid=val, then=pos) for pos, val in enumerate(pk_list)], default=len(pk_list))
+                qs_out = Program.objects.filter(uuid__in=pk_list).order_by(preserved)
+                return qs_out
+            else:
+                return super().get_queryset()
 
 class NoticeViewSet(viewsets.ModelViewSet):
     serializer_class = NoticeSerializer
